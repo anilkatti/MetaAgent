26e885a041cafa3e21902090d7ffb285f2c54fa9
Giancarlo Fissore <giancarlo.fissore@gmail.com>
2024-08-24 19:22:38 -0700

    Add evaluator's implementation - WIP.

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,26 @@
-from utils import _get_model
+from typing import Annotated
+
+from langchain_experimental.utilities import PythonREPL
+
 
 def evaluator_node(state, config):
-    return {"eval_results": ["PASS"]}
+    # TODO: how to extract the code?
+
+    code = "print('Hello AGI house')"
+
+    result = python_repl(code)
+
+    return {"eval_results": [result]}
+
+
+def python_repl(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with ⁠ print(...) ⁠. This is visible to the user."""
+    repl = PythonREPL()
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    return f"PASS.⁠ Stdout:\n{result}"
